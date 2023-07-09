e6e1be12f703eeae5b8bc9d62106e315c3a89e67
Rose De Sicilia <72463845+RoseDeSicilia26@users.noreply.github.com>
2023-07-09 09:54:05 -0700

    cleaned up notebook, prompts, and methods. Added methods to identification and validation class. Added examples of both to notebook
    Signed-off-by: Rose De Sicilia <72463845+RoseDeSicilia26@users.noreply.github.com>

---
+++ sea_ice.py
@@ -0,0 +1,15 @@
+variables : list = [
+    geopotential_heights, 
+    relative_humidity,
+    sea_level_pressure, 
+    zonal_wind_at_10_meters,
+    meridional_wind_at_10_meters,
+    sensible_plus_latent_heat_flux,
+    total_precipitation,
+    total_cloud_cover,
+    total_cloud_water_path, 
+    surface_net_shortwave_flux, 
+    surface_net_longwave_flux,
+    northern_hemisphere_sea_ice_extent
+]
+   
