e71c61b145af55e0414fa88877d5e660cbc21209
Grace Sng <gracie.s.sng@gmail.com>
2025-02-17 13:50:17 -0600

    Reworked simple model suggester and simple identification suggester methods.
    Signed-off-by: Grace Sng <gracie.s.sng@gmail.com>

--- pywhyllm/suggesters/simple_identification_suggester.py
+++ pywhyllm/suggesters/simple_identification_suggester.py
@@ -2,109 +2,160 @@ from typing import List, Tuple, Dict
 from ..protocols import ModelerProtocol
 import networkx as nx
 import guidance
+from guidance import system, user, assistant, gen
 from enum import Enum
 import re
 import itertools
 
+
 class SimpleIdentificationSuggester:
 
-    def suggest_backdoor(
-        self,
-        factors_list,
-        treatment,
-        outcome
-    ):
-        prompt = """
-        {{#system~}} 
-        You are a helpful assistant for causal reasoning.
-        {{~/system}}
-        
-        {{#user~}}
-        Which set or subset of factors in {{factors}} might satisfy the backdoor criteria for identifying the effect of {{treatment}} on {{outcome}}?
+    def __init__(self, llm):
+        if (llm == 'gpt-4'):
+            self.llm = guidance.models.OpenAI('gpt-4')
 
-        List the factors satisfying the backdoor criteria enclosing the name of each factor in <backdoor> </backdoor> tags.
-        {{~/user}}
+    def suggest_iv(self, factors, treatment, outcome):
+        lm = self.llm
+        with system():
+            lm += "You are a helpful assistant for causal reasoning."
 
-        {{#assistant}}
-        {{~gen 'backdoors'}}
-        {{~/assistant}}
-        """ 
-        program = guidance(prompt)
+        with user():
+            lm += f"""Which factors in {factors} might be valid instrumental variables for identifying the effect of {treatment} on {outcome}?
 
-        executed_program = program(factors=str(factors_list), treatment=treatment, outcome=outcome)
+            List the factors that are possible instrumental variables in <iv> </iv> tags."""
+        with assistant():
+            lm += gen("iv")
 
-        if( executed_program._exception is not None):
-            raise executed_program._exception
-        
-        backdoors = executed_program['backdoors']
+        ivs = lm['iv']
+        ivs_list = re.findall(r'<iv>(.*?)</iv>', ivs)
+
+        return ivs_list
+
+    def suggest_backdoor(self, factors, treatment, outcome):
+        lm = self.llm
+        with system():
+            lm += "You are a helpful assistant for causal reasoning."
+
+        with user():
+            lm += f"""Which set or subset of factors in {factors} might satisfy the backdoor criteria for identifying the effect of {treatment} on {outcome}?
+
+            List the factors satisfying the backdoor criteria enclosing the name of each factor in <backdoor> </backdoor> tags.
+            """
+        with assistant():
+            lm += gen("backdoors")
+
+        backdoors = lm['backdoors']
         backdoors_list = re.findall(r'<backdoor>(.*?)</backdoor>', backdoors)
 
         return backdoors_list
 
+    def suggest_frontdoor(self, factors, treatment, outcome):
+        lm = self.llm
+        with system():
+            lm += "You are a helpful assistant for causal reasoning."
 
-    def suggest_frontdoor(
-        self,
-        factors_list,
-        treatment,
-        outcome
-    ):
-        prompt = """
-        {{#system~}} 
-        You are a helpful assistant for causal reasoning.
-        {{~/system}}
-        
-        {{#user~}}
-        Which set or subset of factors in {{factors}} might satisfy the frontdoor criteria for identifying the effect of {{treatment}} on {{outcome}}?
+        with user():
+            lm += f"""Which set or subset of factors in {factors} might satisfy the frontdoor criteria for identifying the effect of {treatment} on {outcome}?
 
-        List the factors satisfying the frontdoor criteria enclosing the name of each factor in <backdoor> </backdoor> tags.
-        {{~/user}}
+            List the factors satisfying the frontdoor criteria enclosing the name of each factor in <frontdoor> </frontdoor> tags.
+            """
+        with assistant():
+            lm += gen("frontdoors")
 
-        {{#assistant}}
-        {{~gen 'frontdoors'}}
-        {{~/assistant}}
-        """ 
-        program = guidance(prompt)
-
-        executed_program = program(factors=str(factors_list), treatment=treatment, outcome=outcome)
-
-        if( executed_program._exception is not None):
-            raise executed_program._exception
-        
-        frontdoors = executed_program['frontdoors']
+        frontdoors = lm['frontdoors']
         frontdoors_list = re.findall(r'<frontdoor>(.*?)</frontdoor>', frontdoors)
 
         return frontdoors_list
 
+    # def suggest_backdoor(
+    #         self,
+    #         factors_list,
+    #         treatment,
+    #         outcome
+    # ):
+    #     prompt = """
+    #     {{#system~}}
+    #     You are a helpful assistant for causal reasoning.
+    #     {{~/system}}
+    #
+    #     {{#user~}}
+    #     Which set or subset of factors in {{factors}} might satisfy the backdoor criteria for identifying the effect of {{treatment}} on {{outcome}}?
+    #
+    #     List the factors satisfying the backdoor criteria enclosing the name of each factor in <backdoor> </backdoor> tags.
+    #     {{~/user}}
+    #
+    #     {{#assistant}}
+    #     {{~gen 'backdoors'}}
+    #     {{~/assistant}}
+    #     """
+    #     program = guidance(prompt)
+    #
+    #     executed_program = program(factors=str(factors_list), treatment=treatment, outcome=outcome)
+    #
+    #     if (executed_program._exception is not None):
+    #         raise executed_program._exception
+    #
+    #     backdoors = executed_program['backdoors']
+    #     backdoors_list = re.findall(r'<backdoor>(.*?)</backdoor>', backdoors)
+    #
+    #     return backdoors_list
 
-    def suggest_iv(
-        self,
-        factors_list,
-        treatment,
-        outcome
-    ):
-        prompt = """
-        {{#system~}} 
-        You are a helpful assistant for causal reasoning.
-        {{~/system}}
-        
-        {{#user~}}
-        Which factors in {{factors}} might be valid instrumental variables for identifying the effect of {{treatment}} on {{outcome}}?
+    # def suggest_frontdoor(
+    #         self,
+    #         factors_list,
+    #         treatment,
+    #         outcome
+    # ):
+    #     prompt = """
+    #     {{#system~}}
+    #     You are a helpful assistant for causal reasoning.
+    #     {{~/system}}
+    #
+    #     {{#user~}}
+    #     Which set or subset of factors in {{factors}} might satisfy the frontdoor criteria for identifying the effect of {{treatment}} on {{outcome}}?
+    #
+    #     List the factors satisfying the frontdoor criteria enclosing the name of each factor in <backdoor> </backdoor> tags.
+    #     {{~/user}}
+    #
+    #     {{#assistant}}
+    #     {{~gen 'frontdoors'}}
+    #     {{~/assistant}}
+    #     """
+    #
+    #     frontdoors = executed_program['frontdoors']
+    #     frontdoors_list = re.findall(r'<frontdoor>(.*?)</frontdoor>', frontdoors)
+    #
+    #     return frontdoors_list
 
-        List the factors that are possible instrumental variables in <iv> </iv> tags.
-        {{~/user}}
-
-        {{#assistant}}
-        {{~gen 'ivs'}}
-        {{~/assistant}}
-        """ 
-        program = guidance(prompt)
-
-        executed_program = program(factors=str(factors_list), treatment=treatment, outcome=outcome)
-
-        if( executed_program._exception is not None):
-            raise executed_program._exception
-        
-        ivs = executed_program['ivs']
-        ivs_list = re.findall(r'<iv>(.*?)</iv>', ivs)
-
-        return ivs_list
+    # def suggest_iv(
+    #         self,
+    #         factors_list,
+    #         treatment,
+    #         outcome
+    # ):
+    #     prompt = """
+    #     {{#system~}}
+    #     You are a helpful assistant for causal reasoning.
+    #     {{~/system}}
+    #
+    #     {{#user~}}
+    #     Which factors in {{factors}} might be valid instrumental variables for identifying the effect of {{treatment}} on {{outcome}}?
+    #
+    #     List the factors that are possible instrumental variables in <iv> </iv> tags.
+    #     {{~/user}}
+    #
+    #     {{#assistant}}
+    #     {{~gen 'ivs'}}
+    #     {{~/assistant}}
+    #     """
+    #     program = guidance(prompt)
+    #
+    #     executed_program = program(factors=str(factors_list), treatment=treatment, outcome=outcome)
+    #
+    #     if (executed_program._exception is not None):
+    #         raise executed_program._exception
+    #
+    #     ivs = executed_program['ivs']
+    #     ivs_list = re.findall(r'<iv>(.*?)</iv>', ivs)
+    #
+    #     return ivs_list
--- pywhyllm/suggesters/simple_model_suggester.py
+++ pywhyllm/suggesters/simple_model_suggester.py
@@ -5,6 +5,9 @@ import guidance
 from enum import Enum
 import re
 import itertools
+import os
+from guidance import system, user, assistant, gen
+
 
 class SimpleModelSuggester:
     """
@@ -21,129 +24,182 @@ class SimpleModelSuggester:
         Suggests the confounding factors that might influence the relationship between a treatment and an outcome, given a list of variables that have already been considered.
     """
 
+    def __init__(self, llm):
+        if (llm == 'gpt-4'):
+            self.llm = guidance.models.OpenAI('gpt-4')
+
+    # new ver
     def suggest_pairwise_relationship(self, variable1: str, variable2: str):
-        """
-        Suggests a cause-and-effect relationship between two variables.
+        lm = self.llm
+        with system():
+            lm += "You are a helpful assistant for causal reasoning."
 
-        Args:
-            variable1 (str): The name of the first variable.
-            variable2 (str): The name of the second variable.
+        with user():
+            lm += f"""Which cause-and-effect-relationship is more likely? First let's very succinctly think about each option. A. {variable1} causes {variable2} B. {variable2} causes {variable1} C. neither {variable1} nor {variable2} cause each other. """
 
-        Returns:
-            list: A list containing the suggested cause variable, the suggested effect variable, and a description of the reasoning behind the suggestion.  If there is no relationship between the two variables, the first two elements will be None.
-        """
-        prompt = """
-        {{#system~}} 
-        You are a helpful assistant for causal reasoning.
-        {{~/system}}
-        
-        {{#user~}}
-        Which cause-and-effect-relationship is more likely?
-        A. {{variable1}} causes {{variable2}}
-        B. {{variable2}} causes {{variable1}}
-        C. neither {{variable1}} nor {{variable2}} cause each other
-        First let's very succinctly think about each option.
-        Then I'll ask you to provide your final answer A, B, or C.
-        {{~/user}}
+        with assistant():
+            lm += gen("description")
 
-        {{#assistant}}
-        {{~gen 'description'}}
-        {{~/assistant}}
+        with user():
+            lm += "Now what is your final answer: A, B, or C? Just give me a single letter."
 
-        {{#user~}}
-        Now what is your final answer: A, B, or C?
-        {{~/user}}
+        with assistant():
+            lm += gen("answer")
 
-        {{#assistant}}
-        {{~#select 'answer'}}A{{~or}}B{{~or}}C{{~/select}}
-        {{~/assistant}}
-        """
+        description = lm['description']
+        answer = lm['answer']
 
-        program = guidance(prompt)
-        executed_program = program(variable1=variable1, variable2=variable2)
-        if( executed_program._exception is not None):
-            raise executed_program._exception
-
-        description = executed_program['description']
-        answer = executed_program['answer']
-
-        if( answer == "A"):
+        if (answer == "A"):
             return [variable1, variable2, description]
-        elif( answer == "B"):
+        elif (answer == "B"):
             return [variable2, variable1, description]
-        elif(answer == "C"):
-            return [None, None, description] # maybe we want to save the description in this case too
+        elif (answer == "C"):
+            return [None, None, description]  # maybe we want to save the description in this case too
         else:
             assert False, "Invalid answer from LLM: " + answer
 
+    # def suggest_pairwise_relationship(self, variable1: str, variable2: str):
+    #     """
+    #     Suggests a cause-and-effect relationship between two variables.
+    #
+    #     Args:
+    #         variable1 (str): The name of the first variable.
+    #         variable2 (str): The name of the second variable.
+    #
+    #     Returns:
+    #         list: A list containing the suggested cause variable, the suggested effect variable, and a description of the reasoning behind the suggestion.  If there is no relationship between the two variables, the first two elements will be None.
+    #     """
+    #     prompt = """
+    #     {{#system~}}
+    #     You are a helpful assistant for causal reasoning.
+    #     {{~/system}}
+    #
+    #     {{#user~}}
+    #     Which cause-and-effect-relationship is more likely?
+    #     A. {{variable1}} causes {{variable2}}
+    #     B. {{variable2}} causes {{variable1}}
+    #     C. neither {{variable1}} nor {{variable2}} cause each other
+    #     First let's very succinctly think about each option.
+    #     Then I'll ask you to provide your final answer A, B, or C.
+    #     {{~/user}}
+    #
+    #     {{#assistant}}
+    #     {{~gen 'description'}}
+    #     {{~/assistant}}
+    #
+    #     {{#user~}}
+    #     Now what is your final answer: A, B, or C?
+    #     {{~/user}}
+    #
+    #     {{#assistant}}
+    #     {{~#select 'answer'}}A{{~or}}B{{~or}}C{{~/select}}
+    #     {{~/assistant}}
+    #     """
+    #
+    #     # program = guidance(prompt)
+    #     # executed_program = program(variable1=variable1, variable2=variable2)
+    #     # if( executed_program._exception is not None):
+    #     #     raise executed_program._exception
+    #     #
+    #     # description = executed_program['description']
+    #     # answer = executed_program['answer']
+    #     #
+    #     # if( answer == "A"):
+    #     #     return [variable1, variable2, description]
+    #     # elif( answer == "B"):
+    #     #     return [variable2, variable1, description]
+    #     # elif(answer == "C"):
+    #     #     return [None, None, description] # maybe we want to save the description in this case too
+    #     # else:
+    #     #     assert False, "Invalid answer from LLM: " + answer
 
-        def suggest_relationships(self, variables: List[str]):
-            """
-            Given a list of variables, suggests relationships between them by querying for pairwise relationships.
-
-            Args:
-                variables (List[str]): A list of variable names.
-
-            Returns:
-                dict: A dictionary of edges found between variables, where the keys are tuples representing the causal relationship between two variables,
-                and the values are the strength of the relationship.
-            """
-            relationships = {}
-            total = (len(variables) * (len(variables)-1)/2)
-            i=0
-            for(var1, var2) in itertools.combinations(variables, 2):
-                i+=1
-                print(f"{i}/{total}: Querying for relationship between {var1} and {var2}")
-                y = self.suggest_pairwise_relationship(var1, var2)
-                if( y[0] == None ):
-                    print(f"\tNo relationship found between {var1} and {var2}")
-                    continue
-                print(f"\t{y[0]} causes {y[1]}")
-                relationships[(y[0], y[1])] = y[2]
-
-            return relationships
-    
-
-    def suggest_confounders(self, variables: List[str], treatment: str, outcome: str) -> List[str]:
+    def suggest_relationships(self, variables: List[str]):
         """
-        Suggests potential confounding factors that might influence the relationship between the treatment and outcome variables.
+        Given a list of variables, suggests relationships between them by querying for pairwise relationships.
 
         Args:
-            variables (List[str]): A list of variables that have already been considered.
-            treatment (str): The name of the treatment variable.
-            outcome (str): The name of the outcome variable.
+            variables (List[str]): A list of variable names.
 
         Returns:
-            List[str]: A list of potential confounding factors.
+            dict: A dictionary of edges found between variables, where the keys are tuples representing the causal relationship between two variables,
+            and the values are the strength of the relationship.
         """
+        relationships = {}
+        total = (len(variables) * (len(variables) - 1) / 2)
+        i = 0
+        for (var1, var2) in itertools.combinations(variables, 2):
+            i += 1
+            print(f"{i}/{total}: Querying for relationship between {var1} and {var2}")
+            y = list(self.suggest_pairwise_relationship(var1, var2))
+            if (y[0] == None):
+                print(f"\tNo relationship found between {var1} and {var2}")
+                continue
+            print(f"\t{y[0]} causes {y[1]}")
+            relationships[(y[0], y[1])] = y[2]
 
-        prompt = """
-        {{#system~}} 
-        You are a helpful assistant for causal reasoning.
-        {{~/system}}
-        
-        {{#user~}}
-        What latent confounding factors might influence the relationship between {{treatment}} and {{outcome}}?
+        return relationships
 
-        We have already considered the following factors {{variables}}.  Please do not repeat them.
+    # new ver
+    def suggest_confounders(self, variables: list[str], treatment: str, outcome: str) -> list[str]:
+        lm = self.llm
 
-        List the confounding factors between {{treatment}} and {{outcome}} enclosing the name of each factor in <conf> </conf> tags.
-        {{~/user}}
+        with system():
+            lm += "You are a helpful assistant for causal reasoning."
 
-        {{#assistant}}
-        {{~gen 'latents'}}
-        {{~/assistant}}
-        """ 
-        program = guidance(prompt)
+        with user():
+            lm += f"""What latent confounding factors might influence the relationship between {treatment} and {outcome}?
 
-        executed_program = program(variables=str(variables), treatment=treatment, outcome=outcome)
+            We have already considered the following factors {variables}.  Please do not repeat them.
 
-        if( executed_program._exception is not None):
-            raise executed_program._exception
-        
-        latents = executed_program['latents']
+            List the confounding factors between {treatment} and {outcome} enclosing the name of each factor in <conf> </conf> tags.
+            """
+        with assistant():
+            lm += gen("latents")
+
+        latents = lm['latents']
         latents_list = re.findall(r'<conf>(.*?)</conf>', latents)
 
         return latents_list
 
 
+# def suggest_confounders(self, variables: List[str], treatment: str, outcome: str) -> List[str]:
+#     """
+#     Suggests potential confounding factors that might influence the relationship between the treatment and outcome variables.
+#
+#     Args:
+#         variables (List[str]): A list of variables that have already been considered.
+#         treatment (str): The name of the treatment variable.
+#         outcome (str): The name of the outcome variable.
+#
+#     Returns:
+#         List[str]: A list of potential confounding factors.
+#     """
+#
+#     prompt = """
+#         {{#system~}}
+#         You are a helpful assistant for causal reasoning.
+#         {{~/system}}
+#
+#         {{#user~}}
+#         What latent confounding factors might influence the relationship between {{treatment}} and {{outcome}}?
+#
+#         We have already considered the following factors {{variables}}.  Please do not repeat them.
+#
+#         List the confounding factors between {{treatment}} and {{outcome}} enclosing the name of each factor in <conf> </conf> tags.
+#         {{~/user}}
+#
+#         {{#assistant}}
+#         {{~gen 'latents'}}
+#         {{~/assistant}}
+#         """
+#     program = guidance(prompt)
+#
+#     executed_program = program(variables=str(variables), treatment=treatment, outcome=outcome)
+#
+#     if (executed_program._exception is not None):
+#         raise executed_program._exception
+#
+#     latents = executed_program['latents']
+#     latents_list = re.findall(r'<conf>(.*?)</conf>', latents)
+#
+#     return latents_list
